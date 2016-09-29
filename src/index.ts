c5834a898d44c4b05d20404c3b336fb76dafe938
Steve Jenkins <steve@skysteve.com>
2016-09-29 16:24:48 +0100

    commenting and tidying

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,64 @@
 import * as glob from 'glob';
 import mkdirp = require('mkdirp');
 import {Schema} from './Schema';
-import {generateTemplate} from './templateHelpers';
+import {generateInterfaceFile} from './templateHelpers';
 
 declare function require(moduleName: string): any;
 
+/**
+ * Recursively create the output directory if it does not exist already
+ * @param directory {string} the output directory to create
+ */
 async function createOutputDir(directory: string): Promise<void> {
-    if (!directory) {
-        return Promise.resolve();
-    }
+  if (!directory) {
+    return Promise.resolve();
+  }
 
-    return new Promise((resolve, reject) => {
-        mkdirp(directory, (err) => {
-            return err ? reject(err) : resolve();
-        });
+  return new Promise((resolve, reject) => {
+    mkdirp(directory, (err) => {
+      return err ? reject(err) : resolve();
     });
+  });
 }
 
+/**
+ * Wrapper around glob to promisify it
+ * @param globPattern {string} the glob pattern to look for
+ * @returns {Promise<Array<string>>} any array of files which match the specified glob pattern
+ */
 async function promisedGlob(globPattern: string): Promise<Array<string>> {
-    return new Promise<Array<string>>((resolve, reject) => {
-        glob(globPattern, (err, files) => {
-            return err ? reject(err) : resolve(files);
-        });
+  return new Promise<Array<string>>((resolve, reject) => {
+    glob(globPattern, (err, files) => {
+      return err ? reject(err) : resolve(files);
     });
+  });
 }
 
+/**
+ * Default export function - create TypeScript interface definitions from the JSON schema files supplied
+ * (Note that interfaces are not created if schemas are not of type object)
+ * @param globPattern {string} the glob pattern to look for schema files
+ * @param outputDirectory {string} the location to write the TypeScript interfaces to
+ * @returns {Promise<Array<string>>} promise chain to create and write interfaces. Promise chain returns file names of the interfaces created
+ */
 export = async function (globPattern: string, outputDirectory: string): Promise<Array<string>> {
-    const files = await Promise.all([
-            promisedGlob(globPattern),
-            createOutputDir(outputDirectory)
-        ])
-        .then(results => results[0]);
+  const files = await Promise.all([
+    promisedGlob(globPattern),
+    createOutputDir(outputDirectory)
+  ])
+    .then(results => results[0]);
 
-    // if no files, nothing to do
-    if (!files || files.length < 1) {
-        console.info('No JSON schema files found');
-        return;
-    }
+  // if no files, nothing to do
+  if (!files || files.length < 1) {
+    console.info('No JSON schema files found');
+    return;
+  }
 
-    const schemas = files.map(file => new Schema(file, require(file)));
+  const schemas = files.map(file => new Schema(file, require(file)));
 
-    return Promise.all(schemas.map(schema => generateTemplate(schema, schemas, outputDirectory)))
-        .then(files => {
-            // remove any empty files
-            return files.filter(filename => !!filename);
-        });
+  return Promise.all(schemas.map(schema => generateInterfaceFile(schema, schemas, outputDirectory)))
+    .then(files => {
+      // remove any empty files
+      return files.filter(filename => !!filename);
+    });
 }
--- src/templateHelpers.ts
+++ src/templateHelpers.ts
@@ -4,95 +4,144 @@ import * as fs from 'fs';
 /// <reference path="../types/left-pad/index.d.ts" />
 import leftPad = require('left-pad');
 
-function getRelatedSchema(arrSchemas: Array<Schema>, relatedId: string) {
-    const relatedSchema = arrSchemas.find(s => s.id === relatedId);
+/**
+ * Find the related schema when referenced from a property
+ * Throws error if schema could not be found
+ * @param arrSchemas {Array<Schema>} the array of schema's we have imported to search through
+ * @param relatedId {String} the ID for the schema we need to find
+ * @returns {Schema} The referenced schema
+ */
+function getRelatedSchema(arrSchemas: Array<Schema>, relatedId: string): Schema {
+  // find schema by ID
+  const relatedSchema = arrSchemas.find(s => s.id === relatedId);
 
-    if (!relatedSchema) {
-        throw new Error(`Could not find referenced schema "${relatedId}"`);
-    }
+  if (!relatedSchema) {
+    throw new Error(`Could not find referenced schema "${relatedId}"`);
+  }
 
-    return relatedSchema;
+  return relatedSchema;
 }
 
-function sortProperties(a: Property, b: Property) {
-    // case insensitive sort
-    const nameA = a.key.toUpperCase();
-    const nameB = b.key.toUpperCase();
+/**
+ * Sort the properties in the outputed interface by key alphabetically (case insensitive)
+ * @param a {Property} the first property to compare
+ * @param b {Property} the second property to compare
+ * @returns {number} 0 if equal, 1 if a should come after b, -1 if b should come after a
+ */
+function sortProperties(a: Property, b: Property): number {
+  // case insensitive sort
+  const nameA = a.key.toUpperCase();
+  const nameB = b.key.toUpperCase();
 
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-        return 1;
-    }
-    return 0;
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
 }
 
+/**
+ * Create module import statements for any referenced schemas (if applicable)
+ * NOTE imports are only created when the referenced schema is of type object.
+ * @param schema {Schema} the schema to generate imports for
+ * @param arrSchemas {Array<Schema>} the array of schemas to search through to find referenced schemas
+ * @returns {string} a string of imports for the supplied schemas (empty string if no imports required)
+ */
 function writeImports(schema: Schema, arrSchemas: Array<Schema>): string {
-    if (schema.dependencies.length < 1) {
-        return '';
-    }
+  if (schema.dependencies.length < 1) {
+    return '';
+  }
 
-    return schema.dependencies.map(dep => getRelatedSchema(arrSchemas, dep))
-            .filter(s => s.type === 'object') // only import objects
-            .map(s => `import {${s.title}} from './${s.outputFileName}';`)
-            .join(('\n')) + '\n\n';
+  return schema.dependencies
+      .map(dep => getRelatedSchema(arrSchemas, dep)) // find the related schema
+      .filter(s => s.type === 'object') // only import if schema is of type object
+      .map(s => `import {${s.title}} from './${s.outputFileName}';`) // generate ES6 module import
+      .join(('\n')) + '\n\n'; // write each import to a new line, leave 2 lines between imports and the interface definition
 }
 
+/**
+ * Write each one of the properties and it's relevant type as detailed in the schema to the interface file recursively
+ * @param schema {Schema} the schema to get the properties from
+ * @param arrSchemas {Array<Schema>} the array of all known schemas used to lookup $ref properties by id
+ * @param indentation {number} the number of spaces to pad from the left (for style only)
+ * @returns {string} the properties of the schema with their associated typescript types
+ */
 function writeProperties(schema: Schema|Property, arrSchemas: Array<Schema>, indentation: number = 2): string {
-    return schema.properties
-        .sort(sortProperties)
-        .map(property => {
-            let result = `${property.key}: `;
-            result = leftPad(result, result.length + indentation);
+  return schema.properties
+    .sort(sortProperties) //sort properties alphabetically
+    .map(property => {
+      // get the key and sort the indentation out
+      let result = `${property.key}: `;
+      result = leftPad(result, result.length + indentation);
 
-            switch (property.typescriptType) {
-                case '___OBJECT___':
-                    result += `{\n${writeProperties(property, arrSchemas, (indentation + 2))}\n  };`;
-                    break;
-                case '___REFERENCE___':
-                    const related = getRelatedSchema(arrSchemas, property.ref);
+      switch (property.typescriptType) {
+        case '___OBJECT___':
+          // if the property is an object, create a child object using it's properties (and indent them an extra 2 spaces)
+          result += `{\n${writeProperties(property, arrSchemas, (indentation + 2))}\n  };`;
+          break;
+        case '___REFERENCE___':
+          // if the property references another schema, find the related schema (this will throw if not found so no need to check)
+          const related = getRelatedSchema(arrSchemas, property.ref);
 
-                    if (related.type === 'object') {
-                        // if the referenced item is another object - reference it's interface
-                        result += `${getRelatedSchema(arrSchemas, property.ref).title};`;
-                    } else {
-                        // if we get a property which isn't an object, convert it to a property and get it's type
-                        const prop = related.toProperty();
-                        result += `${prop.typescriptType};`
-                    }
-                    break;
-                default:
-                result += `${property.typescriptType};`;
-            }
+          if (related.type === 'object') {
+            // if the referenced item is another object - reference it's interface
+            result += `${getRelatedSchema(arrSchemas, property.ref).title};`;
+          } else {
+            // if we get a property which isn't an object, convert it to a property and get it's type
+            const prop = related.toProperty();
+            result += `${prop.typescriptType};`
+          }
+          break;
+        default:
+          // by default write the property type
+          result += `${property.typescriptType};`;
+      }
 
-            // if property is not required add the ? after the key
-            if (!schema.isPropertyRequired(property.key)) {
-                result = result.replace(':', '?:');
-            }
+      // if property is not required add the ? after the key
+      if (!schema.isPropertyRequired(property.key)) {
+        result = result.replace(':', '?:');
+      }
 
-            return result;
-        })
-        .join('\n');
+      return result;
+    })
+    .join('\n'); // write each property on it's own line
 }
 
-async function writeToFile(template: string, schema: Schema, outputDirectory: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        const fileName = `${outputDirectory}/${schema.outputFileName}.d.ts`;
+/**
+ * Do the writing to disk
+ * @param template {string} the generated interface to write to disk
+ * @param schema {Schema} the schema we're writing (used to get the file name)
+ * @param outputDirectory {string} the directory to write the files to (defaults to current directory if not specified)
+ * @returns {Promise<string>} a promise which completes when the file has been written returning the file path of the created file
+ */
+async function writeToFile(template: string, schema: Schema, outputDirectory: string = './'): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const fileName = `${outputDirectory}/${schema.outputFileName}.d.ts`;
 
-        fs.writeFile(fileName, template, (err) => {
-            return err ? reject(err) : resolve(fileName);
-        });
+    fs.writeFile(fileName, template, (err) => {
+      return err ? reject(err) : resolve(fileName);
     });
+  });
 }
 
-export async function generateTemplate(schema: Schema, arrSchemas: Array<Schema>, outputDirectory: string ='./'): Promise<string> {
-    if (schema.type !== 'object') {
-        console.warn(schema.title, 'is not of type object so no interface will be created');
-        return Promise.resolve('');
-    }
+/**
+ * Generate the TypeScript interface file and write it to disk
+ * @param schema {Schema} the schema to generate the interface for
+ * @param arrSchemas {Array<Schema>} the array of all schemas we know about (used to look up related schemas)
+ * @param outputDirectory {String} the directory to write the files to (defaults to current directory if not specified)
+ * @returns {Promise<string>} a promise which completes when the file has been written returning the file path of the created file
+ */
+export async function generateInterfaceFile(schema: Schema, arrSchemas: Array<Schema>, outputDirectory: string): Promise<string> {
+  if (schema.type !== 'object') {
+    // if the schema is not an object schema we can't create an interface for it, just log and return
+    console.warn(schema.title, 'is not of type object so no interface will be created');
+    return Promise.resolve('');
+  }
 
-    const template = `${writeImports(schema, arrSchemas)}export interface ${schema.title} {\n${writeProperties(schema, arrSchemas)}\n}`;
-    return await writeToFile(template, schema, outputDirectory);
+  // generate the template and write it (this is all on 1 line to avoid extra spaces)
+  const template = `${writeImports(schema, arrSchemas)}export interface ${schema.title} {\n${writeProperties(schema, arrSchemas)}\n}`;
+  return await writeToFile(template, schema, outputDirectory);
 }
 
